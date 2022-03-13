b253a8fd6c0acd85acc1afad7e3a48ec7550f7ec
Eugenia <pyssiki@yandex.ru>
2022-03-13 22:30:32 +0500

    changed the description

--- LoginApp/ViewController/LoginViewController.swift
+++ LoginApp/ViewController/LoginViewController.swift
@@ -27,7 +27,6 @@ class LoginViewController: UIViewController {
             } else if let navigationVC = viewController as? UINavigationController {
                 guard let aboutUserVC = navigationVC.topViewController as? UserInfoViewController else { return }
                 aboutUserVC.aboutMeInfo = userData.description
-                aboutUserVC.myPhoto = userData.myPhoto
             } else { return }
         }
     }
